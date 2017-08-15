18df11a814dac74c4152903b235cbe190eceb779
jacr6 <jose.cuevas.cv@gmail.com>
2017-08-15 17:17:54 -0500

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -50,6 +50,7 @@ rm -rf tpl ;
 #185 sed apache
 sed -i 's/None/All/g' /etc/httpd/conf/httpd.conf
 #186 php datetime
+sed -i 's/date\.timezone.*$//g' /etc/php.ini
 sed -i 's/date\.timezone/date.timezone\ndate.timezone\ =\ America\/Guayaquil/g' /etc/php.ini
 #190 - Echo LISTO
 echo "XONE CONNECT INSTALADO";
